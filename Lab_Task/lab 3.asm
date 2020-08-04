d879bcaa9e9527002187c5853926b4181311fa25
WardunNsu <Wardun.noon@northsouth.edu>
2020-08-04 09:26:35 +0600

    Lab 3 assessment

---
+++ Lab_Task/lab 3.asm
@@ -0,0 +1,25 @@
+
+DATA SEGMENT
+    MSG1 DB "Hello World$" 
+    MSG2 DB "Assembly LAnguage CSE331#"
+    
+START:
+    MOV AX, DATA
+    MOV DS, AX
+    LEA DX, MSG1
+     
+    MOV AH, 9
+    INT 21H 
+    
+    MOV AX, DATA
+    MOV DS, AX
+    LEA DX, MSG2
+     
+    MOV AH, 9
+    INT 21H
+    
+    MOV AH, 4cH
+    INT 21H
+      
+    
+END START
