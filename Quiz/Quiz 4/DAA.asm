8967109d9c6ad55e64c58755573e04176d8571be
WardunNsu <Wardun.noon@northsouth.edu>
2020-08-25 08:45:54 +0600

    Quiz 4 submission

---
+++ Quiz/Quiz 4/DAA.asm
@@ -0,0 +1,17 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+org 100h
+
+MOV AL, 27H
+DAA
+
+MOV AL, 35H
+DAA
+
+ret
+
+
+
+
